b8e8c0a4449e2b921e3d0fc06b336613a2434098
jduncan81 <105863015+jduncan81@users.noreply.github.com>
2022-07-24 17:31:47 -0400

    delete folder

--- cleandata.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv("C:/Users/jdunc/JCPSHomelessPopulation/jcps_total_homeless (2).csv")
-
-df.head()
-print(df.head(20))
-
-df.drop('SLN', 'Male', 'Female', 'Homeless % Among Total Homeless Population')

ef66b73384cb62783e019e2f9b52d17280446dfb
jduncan81 <105863015+jduncan81@users.noreply.github.com>
2022-07-28 02:21:07 -0400

    added markdowns, new excel files, deleted csv

--- homeless.py
+++ homeless.py
@@ -66,4 +66,79 @@ print(asian_sum)
 totals=df.iloc[:,range (1,8)].sum()
 print(totals)
 
-totals.plot.pie()
+#JCPS Unsuppressed Homeless Population Data (sum of columns)
+plt.pie
+plt.figure(figsize=(15,15))
+plt.title('JCPS Homeless Population (Unsuppressed)', fontsize=20, fontweight="bold")
+dataframe=pd.DataFrame({"Race": ['Asian', 'Two or More', 'Hispanic', 'White', 'African American'], "Per_Group": [52, 152, 325, 868, 1896]})
+plt.pie(dataframe["Per_Group"], labels =dataframe["Race"], autopct='%1.1f%%',textprops={'fontsize': 15} )
+
+#JCPS Total Homeless Population Data (includes suppressed data)
+plt.pie
+plt.figure(figsize=(15,15))
+plt.title('JCPS Total Homeless Population (Suppressed)', fontsize=20, fontweight="bold")
+dataframe=pd.DataFrame({"Race": ['Asian', 'Two or More', 'Hispanic', 'White', 'African American'], "Per_Group": [112, 228, 561, 940, 2105]})
+plt.pie(dataframe["Per_Group"], labels =dataframe["Race"], autopct='%1.1f%%',textprops={'fontsize': 15} )
+
+#plt.show()
+
+##Check to ensure it dispays correctly
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+#Sort to see which schools have the highest population of homeless students 
+sort_by_total = df.sort_values('Total Homeless Count', ascending=False)
+print(sort_by_total.head(20))
+
+
+elem_enrollment=pd.read_excel("jcps_20-21_enrollment_elem.xlsx")
+#total elementary students enrolled
+elem_total_enrollment=elem_enrollment.iloc[91]
+#total district enrollment (including Pre-K)
+total_district_enrollment=elem_enrollment.iloc[95]
+print(elem_enrollment.describe())
+print(elem_enrollment.info())
+print(elem_enrollment.columns)
+print(elem_enrollment.shape)
+print(elem_enrollment.head())
+print(elem_enrollment.tail(15))
+elem_enrollment.drop(labels=range(91,97), inplace=True)
+print(elem_enrollment.tail())
+
+ms_enrollment=pd.read_excel("jcps_20-21_enrollment_ms.xlsx")
+#total ms students enrolled
+ms_total_enrollment=ms_enrollment.iloc[25]
+print(ms_enrollment.describe())
+print(ms_enrollment.info())
+print(ms_enrollment.columns)
+print(ms_enrollment.shape)
+print(ms_enrollment.head())
+print(ms_enrollment.tail(8))
+ms_enrollment.drop(labels=range(25,31), inplace=True)
+print(ms_enrollment.tail())
+
+hs_enrollment=pd.read_excel("jcps_20-21_enrollment_hs.xlsx")
+#total hs students enrolled
+hs_total_enrollment=hs_enrollment.iloc[21]
+print(hs_enrollment.describe())
+print(hs_enrollment.info())
+print(hs_enrollment.columns)
+print(hs_enrollment.shape)
+print(hs_enrollment.head())
+print(hs_enrollment.tail(8))
+hs_enrollment.drop(labels=range(21,27), inplace=True)
+print(hs_enrollment.tail())
+
+#combined files
+df2=pd.concat([elem_enrollment, ms_enrollment, hs_enrollment])
+print(df2.head())
+print(df2.tail())
+#dropped unwanted columns, leaving school name and total enrollment
+df2.drop(df2.columns.difference(['Unnamed: 1', 'Total']), axis=1, inplace=True)
+print(df2.head())
+
+df.drop(df.columns.difference(['School', 'Total Homeless Count']), axis=1, inplace=True)
+df2.drop(df2.columns.difference(['Unnamed: 1', 'Total']), axis=1, inplace=True)
+print(df, df2)
